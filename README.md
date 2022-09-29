325d09575e965cc848af09d27be72f7f4add883b
vinoth-Kumar-S <31274282+vinoth-Kumar-S@users.noreply.github.com>
2022-09-29 19:14:03 +0530

    Vue Tree Grid selection sample
    Vue Tree Grid selection sample

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# how-to-enable-selection-in-the-vue-tree-grid-component
+# How to enable selection in the Vue Tree Grid Component
+
 A quick-start project that shows how to enable selection options in Vue Tree Grid, and enable different selection types and modes. This project also contains simple code to render checkboxes and toggle selection.
+
+Refer to the following documentation on selection in the Syncfusion Vue Tree Grid component: 
+https://ej2.syncfusion.com/vue/documentation/treegrid/selection/selection/
+
+Check out this online selection example of the Syncfusion Vue Tree Grid component: 
+https://ej2.syncfusion.com/vue/demos/#/bootstrap5/tree-grid/selection.html
+
+Refer to the following Syncfusion Vue Tree Grid getting started video: 
+https://www.youtube.com/watch?v=FEMyOHKjjao
+
+## Project prerequisites
+
+Make sure that you have the latest versions of Vue, Node, Vue Class Component, and Visual Studio Code in your machine before starting to work on this project.
+
+### How to run this application?
+
+To run this application, you need to clone the `how-to-enable-selection-in-the-vue-tree-grid-component` repository and then open it in Visual Studio Code. Now, simply install all the necessary react packages into your current project using the `npm install` command and run your project using the `npm run serve` command.
---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <ejs-button :onclick="btnClick" cssClass="e-btn">Selection API</ejs-button>
+  <ejs-treegrid :dataSource="data" ref="treegridObj"
+    childMapping="subtasks"
+    :treeColumnIndex="2"
+    :selectionSettings="selectionOptions" 
+    :selectedRowIndex="2"
+    :height='400' 
+    :allowPaging='true' 
+    :pageSettings='pageSettings'>
+    <e-columns>
+      <e-column type="Checkbox" width="50"></e-column>
+      <e-column field='taskID' headerText='Task ID' width='80' textAlign='Right'></e-column>
+      <e-column field='taskName' headerText='Task Name' width='190'></e-column>
+      <e-column field='startDate' headerText='Start Date' width='100' format="yMd" textAlign='Right'></e-column>
+      <e-column field='duration' headerText='Duration' width='90' textAlign='Right'></e-column>
+      <e-column field='progress' headerText='Progress' width='90' textAlign='Right'></e-column>
+    </e-columns>
+  </ejs-treegrid>
+</template>
+
+<script>
+import { TreeGridComponent, ColumnsDirective, ColumnDirective, Page} from "@syncfusion/ej2-vue-treegrid";
+import { ButtonComponent} from '@syncfusion/ej2-vue-buttons';
+import {sampleData} from './data.js';
+
+export default{
+  name: 'App',
+  components: {
+    'ejs-treegrid': TreeGridComponent,
+    'e-columns': ColumnsDirective,
+    'e-column': ColumnDirective,
+    'ejs-button': ButtonComponent
+  },
+  data: () => {
+    return {
+      data: sampleData,
+      // mode: "Cell", cellSelectionMode: "Box"
+      selectionOptions: {type: "Multiple", checkboxOnly: false, checkboxMode: "ResetOnRowClick", enableToggle: false},
+      pageSettings: { pageSize: 12 }
+    }
+  },
+  methods: {
+    btnClick: function(){
+      let treegrid = this.$refs.treegridObj.ej2Instances;
+      treegrid.selectRows([5,6,7]);
+      //treegrid.clearSelection(); //To clear the selection
+    }
+  },
+  provide:{
+    treegrid:[Page]
+  }
+}
+</script>
+
+<style>
+  @import "../node_modules/@syncfusion/ej2-base/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-buttons/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-calendars/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-dropdowns/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-inputs/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-navigations/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-popups/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-splitbuttons/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-vue-grids/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-vue-treegrid/styles/material.css";
+
+  .e-btn{
+    margin: 1%;
+  }
+</style>
